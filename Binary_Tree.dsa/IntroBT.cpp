ce29a44e267add55c12e33aa6fcaa22e9726df8d
Yashasvi Dagar <dagaryashu2046@gmail.com>
2025-08-09 10:40:24 +0530

    Tree from LevelOrder

--- Binary_Tree.dsa/IntroBT.cpp
+++ Binary_Tree.dsa/IntroBT.cpp
@@ -47,10 +47,41 @@ void levelOrderTransversal(node* root){
     }
 }
 
+void treeFromLevelOrder(node* &root){
+    queue<node*> q;
+    cout<<"Enter data for root:"<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+    q.push(root);
+    while(!q.empty()){
+        node* temp = q.front();
+        q.pop();
+        cout<<"Enter left node for "<<temp->data<<endl;
+        int leftData;
+        cin>>leftData;
+        if(leftData!=-1){
+            temp->left=new node(leftData);
+            q.push(temp->left);
+        }
+        cout<<"Enter right node for "<<temp->data<<endl;
+        int rightData;
+        cin>>rightData;
+        if(rightData!=-1){
+            temp->right=new node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+
 int main(){
   node* root = NULL;
+    treeFromLevelOrder(root);
+    levelOrderTransversal(root);
+  /*
   //1 2 4 -1 -1 5 -1 -1 3 6 -1 -1 -1
   root = buildTree(root);
   levelOrderTransversal(root);
+  */
   return 0;
 }
