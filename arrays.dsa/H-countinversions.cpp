8e20a64cadcedef7875dde27bc9b93ffd88ec621
Yashasvi Dagar <dagaryashu2046@gmail.com>
2025-11-02 11:30:32 +0530

    COUNT INVERSIONS

---
+++ arrays.dsa/H-countinversions.cpp
@@ -0,0 +1,54 @@
+//Question- Pairs when i<j and arr[i]>arr[j] are called inversions. Count the number of inversions in the array.
+#include <bits/stdc++.h>
+using namespace std;
+int merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; 
+    int left = low;    
+    int right = mid + 1;
+
+    int cnt = 0;
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);//storing in temp array.
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            cnt += (mid - left + 1); //Modification 2
+            right++;
+        }
+    }
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    // transfering elements from temp to arr 
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+    return cnt;
+}
+int mergeSort(vector<int> &arr, int low, int high) {
+    int cnt = 0;
+    if (low >= high) return cnt;
+    int mid = (low + high) / 2 ;
+    cnt += mergeSort(arr, low, mid);  // left half
+    cnt += mergeSort(arr, mid + 1, high); // right half
+    cnt += merge(arr, low, mid, high);  // merging sorted halves
+    return cnt;
+}
+int numberOfInversions(vector<int>&a, int n) {
+    return mergeSort(a, 0, n - 1);
+}
+int main(){
+    vector<int> a = {5, 4, 3, 2, 1};
+    int n = 5;
+    int cnt = numberOfInversions(a, n);
+    cout << "The number of inversions are: "
+         << cnt << endl;
+    return 0;
+}
